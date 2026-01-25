5d727a7ff982de078aa3b88dcd482aa665692b04
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-01-25 15:39:26 -0500

    abstract class

---
+++ abstract-class/abstract_class_ecommerce.py
@@ -0,0 +1,19 @@
+"""
+Docstring for abstract-class.abstract_class_ecommerce
+Scenario: Order Processing System (E-commerce)
+
+You are building an order processing engine for an e-commerce platform.
+
+Different order types exist:
+
+DigitalOrder (ebooks, subscriptions)
+
+PhysicalOrder (clothes, electronics)
+
+InternationalOrder (customs, duties)
+
+Each order follows a common lifecycle, but some steps differ depending on the order type.
+"""
+
+
+
