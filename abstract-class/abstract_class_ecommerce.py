61c7791e8011e83c71390b0ad2bcffdcaa074c71
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-01-25 16:03:23 -0500

    lld problems

--- abstract-class/abstract_class_ecommerce.py
+++ abstract-class/abstract_class_ecommerce.py
@@ -14,6 +14,81 @@ InternationalOrder (customs, duties)
 
 Each order follows a common lifecycle, but some steps differ depending on the order type.
 """
+from abc import ABC, abstractmethod
+
+
+class OrderProcessor(ABC):
+    """
+    Docstring for OrderProcessor
+    The OrderProcessor class has the blue print for the order processing
+    """
+
+    def __init__(self, order_id, amount):
+
+        self.order_id = order_id
+        self.amount = amount
+
+    @abstractmethod
+    def validate_order(self):
+
+        pass
+
+    @abstractmethod
+    def calculate_total(self):
+
+        pass
+
+    @abstractmethod
+    def process_payment(self):
+
+        pass
+
+    def apply_discount(self):
+
+        pass
+
+    def generate_invoice(self):
+
+        pass
 
 
 
+
+class DigitalOrder(OrderProcessor):
+
+    def __init__(self):
+
+        self.shipping_cost = False
+        
+    
+    def apply_discount(self):
+        
+        return "Can apply digital discount"
+    
+
+
+class PhysicalOrder(OrderProcessor):
+
+    def __init__(self):
+
+        self.shipping_cost = True
+        
+    
+    def apply_discount(self):
+        
+        return "Can apply digital discount"
+    
+
+
+class InternationalOrder(OrderProcessor,PhysicalOrder):
+
+    def __init__(self):
+        
+        self.custom_duty = True
+
+
+    def generate_invoice(self):
+
+        return "Add custom Duty"
+
+        
