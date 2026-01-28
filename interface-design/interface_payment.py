b156c4350c1a6025f4ccc150c1c1ae6de2fe3890
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-01-27 21:50:04 -0500

    interface code

---
+++ interface-design/interface_payment.py
@@ -0,0 +1,18 @@
+"""
+The progrom to make the basic of the interface design
+
+
+We’ll build a Payment System using:
+
+Interface (contract)
+
+Abstract class (shared behavior)
+
+Concrete implementations
+
+Client code that depends only on the interface
+
+
+
+"""
+
