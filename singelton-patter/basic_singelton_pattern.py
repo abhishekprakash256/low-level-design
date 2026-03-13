b843daf1690f679dce24d2b2e440b98dfe4008e2
abhishekprakash256 <abhishekprakash47@gmail.com>
2026-03-12 23:18:40 -0400

    test code

--- singelton-patter/basic_singelton_pattern.py
+++ singelton-patter/basic_singelton_pattern.py
@@ -10,6 +10,12 @@ class SingeltonClass():
 		return f"This is the {word}"
 
 
+class SingeltonClassFixed():
+
+	def test_func(self) :
+
+		return "This is the test"
 
 singelton_class = SingeltonClass()
 
+singelton_class_fixed = SingeltonClassFixed()
--- singelton-patter/test_singelton.py
+++ singelton-patter/test_singelton.py
@@ -3,7 +3,7 @@ The file to use the singelton
 """
 
 
-from basic_singelton_pattern import singelton_class
+from basic_singelton_pattern import singelton_class , singelton_class_fixed
 
 
 print(singelton_class.test_func("hello"))
@@ -11,3 +11,7 @@ print(singelton_class.test_func("hello"))
 print(singelton_class.test_func("Hey"))
 
 print(singelton_class.test_func("hello") == singelton_class.test_func("Hey"))
+
+print(singelton_class_fixed.test_func())
+
+print(singelton_class_fixed.test_func() == singelton_class_fixed.test_func())
